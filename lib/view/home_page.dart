379049095b2b1d0ed32883d68bfb1491a18d2ab9
zhouyuan <z244370114@outlook.com>
2024-01-18 19:54:54 +0800

    1.更新代码；

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -8,10 +8,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final List<NavigationPaneItem> items = [
+    PaneItemHeader(header: Text("推荐")),
+    PaneItem(
+      icon: const Icon(FluentIcons.home),
+      title: const Text('热门博文'),
+      body: _NavigationBodyItem(),
+    ),
+    PaneItem(
+      icon: const Icon(FluentIcons.home),
+      title: const Text('每日一问'),
+      body: _NavigationBodyItem(),
+    ),
+    PaneItemHeader(header: Text("专题")),
+    PaneItem(
+      icon: const Icon(FluentIcons.home),
+      title: const Text('面试相关'),
+      body: _NavigationBodyItem(),
+    ),
+    PaneItem(
+      icon: const Icon(FluentIcons.home),
+      title: const Text('性能优化'),
+      body: _NavigationBodyItem(),
+    ),
+  ];
+
+  var topIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return NavigationView(
+      pane: NavigationPane(
+        selected: topIndex,
+        onChanged: (index) => setState(() => topIndex = index),
+        displayMode: PaneDisplayMode.auto,
+        items: items,
+      ),
+    );
   }
 
+  Widget _itemWidget() {
+    return Container(
+      child: Row(
+        children: [
+          Icon(FluentIcons.favorite_star, size: 40),
+          Column(
+            children: [
+              Text("data"),
+              Row(
+                children: [
+
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+}
+
+class _NavigationBodyItem extends StatelessWidget {
+  const _NavigationBodyItem({
+    this.header,
+    this.content,
+  });
+
+  final String? header;
+  final Widget? content;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldPage.withPadding(
+      // header: PageHeader(title: Text(header ?? 'This is a header text')),
+      content: content ?? const SizedBox.shrink(),
+    );
+  }
 }
