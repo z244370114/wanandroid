44611cbc5c78c6d2cc8de04c8f048b60d3c5aab7
zhouyuan <z244370114@outlook.com>
2024-01-18 17:47:08 +0800

    1.添加常用库； 2.首页界面UI布局；

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,459 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/link.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+import 'theme.dart';
+import 'utils/platform_utils.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+
+import 'view/home_page.dart';
+
+const String appTitle = 'wanandroid';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (PlatformUtils.isDesktop) {
+    await flutter_acrylic.Window.initialize();
+    await flutter_acrylic.Window.hideWindowControls();
+    await WindowManager.instance.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitleBarStyle(
+        TitleBarStyle.hidden,
+        windowButtonVisibility: false,
+      );
+      await windowManager.setMinimumSize(const Size(500, 600));
+      await windowManager.show();
+      await windowManager.setPreventClose(true);
+      await windowManager.setSkipTaskbar(false);
+    });
+  }
+
   runApp(const MyApp());
 }
+
+final _appTheme = AppTheme();
+
+final rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
+final GoRouter router = GoRouter(
+  navigatorKey: rootNavigatorKey,
+  routes: <RouteBase>[
+    ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) {
+          return MyHomePage(
+            shellContext: _shellNavigatorKey.currentContext,
+            child: child,
+          );
+        },
+        routes: [
+          GoRoute(path: '/', builder: (context, state) => const HomePage()),
+        ]),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ChangeNotifierProvider.value(
+      value: _appTheme,
+      builder: (context, child) {
+        final appTheme = context.watch<AppTheme>();
+        return FluentApp.router(
+          title: "wanandroid",
+          themeMode: appTheme.mode,
+          debugShowCheckedModeBanner: false,
+          color: appTheme.color,
+          darkTheme: FluentThemeData(
+            brightness: Brightness.dark,
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+            ),
+          ),
+          theme: FluentThemeData(
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+            ),
+          ),
+          locale: appTheme.locale,
+          builder: (context, child) {
+            return Directionality(
+              textDirection: appTheme.textDirection,
+              child: NavigationPaneTheme(
+                data: NavigationPaneThemeData(
+                  backgroundColor: appTheme.windowEffect !=
+                          flutter_acrylic.WindowEffect.disabled
+                      ? Color(0x00000000)
+                      : null,
+                ),
+                child: child!,
+              ),
+            );
+          },
+          routeInformationParser: router.routeInformationParser,
+          routerDelegate: router.routerDelegate,
+          routeInformationProvider: router.routeInformationProvider,
+        );
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({
+    super.key,
+    required this.child,
+    required this.shellContext,
+  });
 
-  final String title;
+  final Widget child;
+  final BuildContext? shellContext;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>  with WindowListener {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final viewKey = GlobalKey(debugLabel: 'Navigation View Key');
+
+
+  late final List<NavigationPaneItem> originalItems = [
+    PaneItem(
+      key: const ValueKey('/'),
+      icon: const Icon(FluentIcons.home),
+      title: const Text('首页'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/inputs/buttons'),
+      icon: const Icon(FluentIcons.button_control),
+      title: const Text('学习路线'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/inputs/checkbox'),
+      icon: const Icon(FluentIcons.checkbox_composite),
+      title: const Text('广场'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/inputs/slider'),
+      icon: const Icon(FluentIcons.slider),
+      title: const Text('导航'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/inputs/toggle_switch'),
+      icon: const Icon(FluentIcons.toggle_left),
+      title: const Text('教程'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItemSeparator(),
+    PaneItem(
+      key: const ValueKey('/forms/text_box'),
+      icon: const Icon(FluentIcons.text_field),
+      title: const Text('问答'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/forms/auto_suggest_box'),
+      icon: const Icon(FluentIcons.page_list),
+      title: const Text('项目'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/forms/combobox'),
+      icon: const Icon(FluentIcons.combobox),
+      title: const Text('公众号'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/forms/numberbox'),
+      icon: const Icon(FluentIcons.number),
+      title: const Text('项目分类'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/forms/passwordbox'),
+      icon: const Icon(FluentIcons.password_field),
+      title: const Text('工具'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      key: const ValueKey('/forms/time_picker'),
+      icon: const Icon(FluentIcons.time_picker),
+      title: const Text('收藏'),
+      body: const SizedBox.shrink(),
+    ),
+  ].map((e) {
+    if (e is PaneItem) {
+      return PaneItem(
+        key: e.key,
+        icon: e.icon,
+        title: e.title,
+        body: e.body,
+        onTap: () {
+          final path = (e.key as ValueKey).value;
+          if (GoRouterState.of(context).uri.toString() != path) {
+            context.go(path);
+          }
+          e.onTap?.call();
+        },
+      );
+    }
+    return e;
+  }).toList();
+
+  late final List<NavigationPaneItem> footerItems = [
+    PaneItemSeparator(),
+    PaneItem(
+      key: const ValueKey('/settings'),
+      icon: const Icon(FluentIcons.settings),
+      title: const Text('设置'),
+      body: const SizedBox.shrink(),
+      onTap: () {
+        if (GoRouterState.of(context).uri.toString() != '/settings') {
+          context.go('/settings');
+        }
+      },
+    ),
+    _LinkPaneItemAction(
+      icon: const Icon(FluentIcons.open_source),
+      title: const Text('Source code'),
+      link: 'https://github.com/bdlukaa/fluent_ui',
+      body: const SizedBox.shrink(),
+    ),
+  ];
+
+  int _calculateSelectedIndex(BuildContext context) {
+    final location = GoRouterState.of(context).uri.toString();
+    int indexOriginal = originalItems
+        .where((item) => item.key != null)
+        .toList()
+        .indexWhere((item) => item.key == Key(location));
+
+    if (indexOriginal == -1) {
+      int indexFooter = footerItems
+          .where((element) => element.key != null)
+          .toList()
+          .indexWhere((element) => element.key == Key(location));
+      if (indexFooter == -1) {
+        return 0;
+      }
+      return originalItems
+          .where((element) => element.key != null)
+          .toList()
+          .length +
+          indexFooter;
+    } else {
+      return indexOriginal;
+    }
   }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    final localizations = FluentLocalizations.of(context);
+    final appTheme = context.watch<AppTheme>();
+    final theme = FluentTheme.of(context);
+
+    return NavigationView(
+      key: viewKey,
+      appBar: NavigationAppBar(
+        automaticallyImplyLeading: false,
+        leading: () {
+          final enabled = widget.shellContext != null && router.canPop();
+
+          final onPressed = enabled
+              ? () {
+            if (router.canPop()) {
+              context.pop();
+              setState(() {});
+            }
+          }
+              : null;
+          return NavigationPaneTheme(
+            data: NavigationPaneTheme.of(context).merge(NavigationPaneThemeData(
+              unselectedIconColor: ButtonState.resolveWith((states) {
+                if (states.isDisabled) {
+                  return ButtonThemeData.buttonColor(context, states);
+                }
+                return ButtonThemeData.uncheckedInputColor(
+                  FluentTheme.of(context),
+                  states,
+                ).basedOnLuminance();
+              }),
+            )),
+            child: Builder(
+              builder: (context) => PaneItem(
+                icon: const Center(child: Icon(FluentIcons.back, size: 12.0)),
+                title: Text(localizations.backButtonTooltip),
+                body: const SizedBox.shrink(),
+                enabled: enabled,
+              ).build(
+                context,
+                false,
+                onPressed,
+                displayMode: PaneDisplayMode.compact,
+              ),
+            ),
+          );
+        }(),
+        title: () {
+          if (kIsWeb) {
+            return const Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(appTitle),
+            );
+          }
+          return const DragToMoveArea(
+            child: Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(appTitle),
+            ),
+          );
+        }(),
+        actions: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Align(
+            alignment: AlignmentDirectional.centerEnd,
+            child: Padding(
+              padding: const EdgeInsetsDirectional.only(end: 8.0),
+              child: ToggleSwitch(
+                content: const Text('Dark Mode'),
+                checked: FluentTheme.of(context).brightness.isDark,
+                onChanged: (v) {
+                  if (v) {
+                    appTheme.mode = ThemeMode.dark;
+                  } else {
+                    appTheme.mode = ThemeMode.light;
+                  }
+                },
+              ),
+            ),
+          ),
+          if (!kIsWeb) const WindowButtons(),
+        ]),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      paneBodyBuilder: (item, child) {
+        final name =
+        item?.key is ValueKey ? (item!.key as ValueKey).value : null;
+        return FocusTraversalGroup(
+          key: ValueKey('body$name'),
+          child: widget.child,
+        );
+      },
+      pane: NavigationPane(
+        selected: _calculateSelectedIndex(context),
+        header: SizedBox(
+          height: kOneLineTileHeight,
+          child: ShaderMask(
+            shaderCallback: (rect) {
+              final color = appTheme.color.defaultBrushFor(
+                theme.brightness,
+              );
+              return LinearGradient(
+                colors: [
+                  color,
+                  color,
+                ],
+              ).createShader(rect);
+            },
+            child: const FlutterLogo(
+              style: FlutterLogoStyle.horizontal,
+              size: 80.0,
+              textColor: Colors.white,
+              duration: Duration.zero,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
+        displayMode: appTheme.displayMode,
+        indicator: () {
+          switch (appTheme.indicator) {
+            case NavigationIndicators.end:
+              return const EndNavigationIndicator();
+            case NavigationIndicators.sticky:
+            default:
+              return const StickyNavigationIndicator();
+          }
+        }(),
+        items: originalItems,
+        autoSuggestBoxReplacement: const Icon(FluentIcons.search),
+        footerItems: footerItems,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final FluentThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 50,
+      child: WindowCaption(
+        brightness: theme.brightness,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
+
+class _LinkPaneItemAction extends PaneItem {
+  _LinkPaneItemAction({
+    required super.icon,
+    required this.link,
+    required super.body,
+    super.title,
+  });
+
+  final String link;
+
+  @override
+  Widget build(
+      BuildContext context,
+      bool selected,
+      VoidCallback? onPressed, {
+        PaneDisplayMode? displayMode,
+        bool showTextOnTop = true,
+        bool? autofocus,
+        int? itemIndex,
+      }) {
+    return Link(
+      uri: Uri.parse(link),
+      builder: (context, followLink) => Semantics(
+        link: true,
+        child: super.build(
+          context,
+          selected,
+          followLink,
+          displayMode: displayMode,
+          showTextOnTop: showTextOnTop,
+          itemIndex: itemIndex,
+          autofocus: autofocus,
+        ),
       ),
     );
   }
---
+++ lib/view/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
